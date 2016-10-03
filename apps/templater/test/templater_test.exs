0b02e0ffcc3deedfb1d1e64ee3c0791306996574
Lexi Huefner <lexi.huefner@i27960-IS3.local>
2016-10-03 15:03:51 -0600

    added templater test

--- apps/templater/test/templater_test.exs
+++ apps/templater/test/templater_test.exs
@@ -2,7 +2,12 @@ defmodule TemplaterTest do
   use ExUnit.Case
   doctest Templater
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  require EEx
+
+  test "When render function is called with a map and EEx template it returns a rendered string" do
+  	inputData = %{:name => "Jasper", :age => 8}
+  	inputTemplate = "Hello <%= @name %>, you're <%= @age %>!"
+
+    assert "Hello Jasper, you're 8!" == Templater.render(inputData, inputTemplate)
   end
 end
