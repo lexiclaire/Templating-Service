d0635b00f89c04ebaafba7bbba61bf556ddaed79
Lexi Huefner <lexi.huefner@i27960-IS3.local>
2016-10-11 13:25:50 -0600

    new test for entire service functionality

--- apps/service/test/controllers/templater_controller_test.exs
+++ apps/service/test/controllers/templater_controller_test.exs
@@ -1,10 +1,13 @@
 defmodule Service.TemplaterControllerTest do
-  use ExUnit.Case
-  use Plug.Test
-  alias TemplaterController.Router
+	use ExUnit.Case
 
-  test "Can I get a connection(?)" do
-    response = conn(:post, "/render")
-    assert response.status == 200
+	require EEx
+
+  test "When I give my app data and a template string it accepts it and renders a completed template string" do
+    
+    inputData = %{:name => "Jasper", :age => 8}
+  	inputTemplate = "Hello <%= @name %>, you're <%= @age %>!"
+
+    assert "Hello Jasper, you're 8!" == "?"
   end
 end
