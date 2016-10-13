096385b8972f7ad4e1ae8029f59622967b90a7d0
Lexi Huefner <lexi.huefner@i27960-IS3.local>
2016-10-13 17:37:10 -0600

    Oops, forgot to fix the test

--- apps/templater/test/templater_test.exs
+++ apps/templater/test/templater_test.exs
@@ -6,7 +6,7 @@ defmodule TemplaterTest do
 
   test "When render function is called with a map and EEx template it returns a rendered string" do
   	inputData = %{"name" => "Jasper", "age" => 8}
-  	inputTemplate = ~s(Hello <%= map["name"] %>, you're <%= map["how_old"] %>!)
+  	inputTemplate = ~s(Hello <%= map["name"] %>, you're <%= map["age"] %>!)
 
     assert "Hello Jasper, you're 8!" == Templater.render(inputTemplate, inputData)
   end
