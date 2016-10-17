5dd1314cd3d0263b91cfcf7cbd9bcc9bae039eee
Lexi Huefner <lexi.huefner@i27960-IS3.local>
2016-10-17 10:11:43 -0600

    Added logging for IP address of client and size of template

--- apps/service/web/controllers/templater_controller.ex
+++ apps/service/web/controllers/templater_controller.ex
@@ -1,9 +1,11 @@
 defmodule Service.TemplaterController do
+	require Logger
   use Service.Web, :controller
   alias Templater
 
   def render(conn, %{"template" => template, "data" => data}) do 
-  	IO.inspect data
-  	json(conn, Templater.render(template, data)) 
+  	Logger.info "Client IP: #{conn.remote_ip |> Tuple.to_list |> Enum.join(".")}, " <> 
+  		"Template length: #{String.length(template)}"
+  	json(conn, %{"rendered" => Templater.render(template, data)}) 
   end
 end
